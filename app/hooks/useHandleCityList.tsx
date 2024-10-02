0d1b86a89014f8b6f1c81d13581d636b5d3d6445
Ayako <ayakonagane@gmail.com>
2024-10-03 12:44:03 +1300

    dlete button workign

--- app/hooks/useHandleCityList.tsx
+++ app/hooks/useHandleCityList.tsx
@@ -2,14 +2,40 @@ import { View, Text } from "react-native";
 import React from "react";
 import { WeatherData } from "../types/forcastType";
 import { useState } from "react";
-import { getDocs, collection } from "firebase/firestore";
+import {
+  getDocs,
+  collection,
+  doc,
+  deleteDoc,
+  query,
+  where,
+} from "firebase/firestore";
 import { firestore } from "../firebaseConfig";
 
 const useHandleCityList = () => {
   const [cities, setCities] = useState<WeatherData[]>([]);
-  const handleDelete = (city: string, country: string) => {
-    console.log(city);
-    console.log(country);
+  const handleDelete = async (city: string, country: string) => {
+    const q = query(
+      collection(firestore, "weatherData2"),
+      where("location.name", "==", city),
+      where("location.country", "==", country)
+    );
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach(async (document) => {
+      await deleteDoc(doc(firestore, "weatherData2", document.id));
+      console.log(`Document with ID ${document.id} deleted`);
+      const weatherDataRef = collection(firestore, "weatherData2");
+      const querySnapshot = await getDocs(weatherDataRef);
+
+      // Convert `querySnapshot` to an array of objects, ensuring it's typed correctly
+      const storedCities: WeatherData[] = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+      })) as WeatherData[];
+
+      // Update state with the fetched cities
+      setCities(storedCities);
+    });
   };
   const fetchCityList = async () => {
     try {
