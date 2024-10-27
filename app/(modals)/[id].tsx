f9524bd32dda1bb6ef29bee0c007ce43a68a9876
multiahfoon <multiahfoon@gmail.com>
2024-10-28 12:52:39 +1300

    refactor: remove imports not being used

--- app/(modals)/[id].tsx
+++ app/(modals)/[id].tsx
@@ -1,29 +1,16 @@
-import {
-  View,
-  Text,
-  ScrollView,
-  ImageBackground,
-  StyleSheet,
-  Button,
-} from "react-native";
-import React from "react";
-import { useLocalSearchParams } from "expo-router";
-import { useState, useEffect } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { firestore } from "../firebaseConfig";
-import useGetImage from "../hooks/useGetImage";
-import useShare from "../hooks/useShare";
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
 
 //this page is only if we want detail page for each information of the city (each information desplayed is touchableOpacity but maybe we dont need that far)
 
 const Page = () => {
-  const { id } = useLocalSearchParams<{ id: any }>();
-  return (
-    <View>
-      <Text>{id}</Text>
-    </View>
-  );
-};
+	const { id } = useLocalSearchParams<{ id: any }>()
+	return (
+		<View>
+			<Text>{id}</Text>
+		</View>
+	)
+}
 
-export default Page;
+export default Page
