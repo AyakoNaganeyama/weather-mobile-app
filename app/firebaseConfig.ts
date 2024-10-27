226d8a33a684963876852e0d273896fd70cead60
multiahfoon <multiahfoon@gmail.com>
2024-10-28 12:51:25 +1300

    refactor: config values

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -1,27 +1,27 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
 
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-import {getStorage} from "firebase/storage";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDwx7RZruIv4C-pFfTDp2MEB-xjGsy7ih8",
-  authDomain: "weather-app-updated.firebaseapp.com",
-  projectId: "weather-app-updated",
-  storageBucket: "weather-app-updated.appspot.com",
-  messagingSenderId: "719386058797",
-  appId: "1:719386058797:web:fb0394a7fa337bde9018a4"
-};
+	apiKey: 'AIzaSyB2Gf7cl-eBWLBgBNSPBaiIceOE4FfcnmU',
+	authDomain: 'weather-app-demo-f462a.firebaseapp.com',
+	projectId: 'weather-app-demo-f462a',
+	storageBucket: 'weather-app-demo-f462a.appspot.com',
+	messagingSenderId: '550010739355',
+	appId: '1:550010739355:web:f7c52a63f4688d2a34843e',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const firestore = getFirestore(app);
-const storage = getStorage(app);
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const firestore = getFirestore(app)
+const storage = getStorage(app)
 
-export {app,auth,firestore,storage}
+export { app, auth, firestore, storage }
