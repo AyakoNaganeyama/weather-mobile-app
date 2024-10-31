8dd21e354db29c44f39689e3e0f7d369f6082b20
multiahfoon <multiahfoon@gmail.com>
2024-11-01 09:53:06 +1300

    refactor: remove imports not being used

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,18 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { Tabs } from "expo-router";
-import { TabBar } from "@/components/TabBar";
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { TabBar } from '@/components/TabBar'
 
 const Layout = () => {
-  return (
-    // passing props for styling for bottom tab bar
-    <Tabs tabBar={(props) => <TabBar {...props} />}>
-      <Tabs.Screen name="index" options={{ headerShown: false }}></Tabs.Screen>
-      <Tabs.Screen
-        name="explore"
-        options={{ headerShown: false }}
-      ></Tabs.Screen>
-    </Tabs>
-  );
-};
+	return (
+		// passing props for styling for bottom tab bar
+		<Tabs tabBar={(props) => <TabBar {...props} />}>
+			<Tabs.Screen name='index' options={{ headerShown: false }}></Tabs.Screen>
+			<Tabs.Screen
+				name='explore'
+				options={{ headerShown: false }}
+			></Tabs.Screen>
+		</Tabs>
+	)
+}
 
-export default Layout;
+export default Layout
