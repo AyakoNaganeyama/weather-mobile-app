6f98f8b9d8ea5b385eb0738c0babc786c85ceb2c
multiahfoon <multiahfoon@gmail.com>
2024-11-01 21:16:35 +1300

    refactor: remove convertHours and import as util

--- app/hooks/useHandleSearch.tsx
+++ app/hooks/useHandleSearch.tsx
@@ -29,6 +29,7 @@ import useIsExist from '../stores/isExist'
 import useHandleCityList from './useHandleCityList'
 import useAucklandWeather from '../stores/aucklandImageStore'
 import { HourForecast } from '../types/HourForecast'
+import { convertHours } from '@/util/convertHours'
 
 // this contains inital search (current location weather ) and search for other cities functions
 
@@ -145,29 +146,6 @@ const useHandleSearch = () => {
 
 			getWeather()
 		}
-	}
-
-	// this is helper for converting to am and pm format
-	const convertHours = (forecasts: HourForecast[]): HourForecast[] => {
-		const convertEpochTo12Hour = (epoch: number): string => {
-			const date = new Date(epoch * 1000) // Convert from seconds to milliseconds
-			let hours = date.getHours() // Get the hour in 24-hour format
-			const minutes = date.getMinutes() // Get the minutes
-			const ampm = hours >= 12 ? 'PM' : 'AM' // Determine AM or PM
-			hours = hours % 12 // Convert to 12-hour format
-			hours = hours ? hours : 12 // If hours is 0 (midnight), make it 12
-
-			// Format minutes with leading zero if needed
-			const minutesStr = minutes < 10 ? '0' + minutes : minutes
-
-			return `${hours}:${minutesStr} ${ampm}`
-		}
-
-		// Return a new array with only `time_epoch` converted
-		return forecasts.map((forecast) => ({
-			...forecast, // Keep all other properties the same
-			time_epoch: convertEpochTo12Hour(forecast.time_epoch as number), // Convert `time_epoch` to 12-hour AM/PM format
-		}))
 	}
 
 	// connected to search button in explore.tsx
