b84c1e6f0cd85744ad15f95fb05a97ed69abde32
multiahfoon <multiahfoon@gmail.com>
2024-11-02 16:03:44 +1300

    style: single line export with self close tags

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,17 +2,12 @@ import React from 'react'
 import { Tabs } from 'expo-router'
 import { TabBar } from '@/components/TabBar'
 
-const Layout = () => {
+export default function Layout() {
 	return (
 		// passing props for styling for bottom tab bar
 		<Tabs tabBar={(props) => <TabBar {...props} />}>
-			<Tabs.Screen name='index' options={{ headerShown: false }}></Tabs.Screen>
-			<Tabs.Screen
-				name='explore'
-				options={{ headerShown: false }}
-			></Tabs.Screen>
+			<Tabs.Screen name='index' options={{ headerShown: false }} />
+			<Tabs.Screen name='explore' options={{ headerShown: false }} />
 		</Tabs>
 	)
 }
-
-export default Layout
