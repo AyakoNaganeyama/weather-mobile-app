1b501eccc6218bbc8e9f57163bdeb1cc7bb55cb9
multiahfoon <multiahfoon@gmail.com>
2024-11-02 13:32:08 +1300

    refactor: remove imports not being used

--- app/hooks/useFrontEndLogic.tsx
+++ app/hooks/useFrontEndLogic.tsx
@@ -1,5 +1,3 @@
-import { View, Text } from 'react-native'
-import React from 'react'
 // those functions are to display text info for detail weather info
 const useFrontEndLogic = () => {
 	const checkUV = (n: number) => {
