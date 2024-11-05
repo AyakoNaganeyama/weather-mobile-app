7534fe9304373d6253cda3876e49bd8b874f6dfe
multiahfoon <multiahfoon@gmail.com>
2024-11-05 20:00:31 +1300

    feat: add covertToDay unit test

---
+++ util/_tests_/convertToDay.test.ts
@@ -0,0 +1,13 @@
+import { covertToDay } from '../convertToDay'
+
+describe('covertToDay', () => {
+	test('convert date format into day of the week example Mon', () => {
+		expect(covertToDay('2024-11-04')).toBe('Mon')
+		expect(covertToDay('2024-11-05')).toBe('Tue')
+		expect(covertToDay('2024-11-06')).toBe('Wed')
+		expect(covertToDay('2024-11-07')).toBe('Thu')
+		expect(covertToDay('2024-11-08')).toBe('Fri')
+		expect(covertToDay('2024-11-09')).toBe('Sat')
+		expect(covertToDay('2024-11-10')).toBe('Sun')
+	})
+})
