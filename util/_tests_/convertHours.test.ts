3e0b54a0b9fac0be5f50d5d74c77cd751a46b966
multiahfoon <multiahfoon@gmail.com>
2024-11-06 23:49:18 +1300

    refactor: simplify unit test
    the function is now more complex and take into account regions

--- util/_tests_/convertHours.test.ts
+++ util/_tests_/convertHours.test.ts
@@ -1,56 +1,20 @@
 import { convertHours } from '../convertHours'
-import { HourForecast } from '@/types/HourForecast'
 
 describe('convertHours unit test', () => {
-	it('should convert time_epoch to AM/PM format', () => {
-		const mockData: HourForecast[] = [
+	it('convert time_epoch to AM/PM format', () => {
+		const currentEpochTime = Math.floor(Date.now() / 1000) // convert date into seconds
+
+		const mockData: any[] = [
 			{
-				time_epoch: 1684200000, // timestamp
-				time: '2023-06-15 13:20',
-				temp_c: 25,
-				temp_f: 77,
-				is_day: 1,
-				condition: { text: 'Sunny', icon: '_', code: 1000 },
-				wind_mph: 10,
-				wind_kph: 16,
-				wind_degree: 180,
-				wind_dir: 'S',
-				pressure_mb: 1015,
-				pressure_in: 30.0,
-				precip_mm: 0,
-				precip_in: 0,
-				snow_cm: 0,
-				humidity: 50,
-				cloud: 0,
-				feelslike_c: 25,
-				feelslike_f: 77,
-				windchill_c: 25,
-				windchill_f: 77,
-				heatindex_c: 25,
-				heatindex_f: 77,
-				dewpoint_c: 10,
-				dewpoint_f: 50,
-				will_it_rain: 0,
-				chance_of_rain: 0,
-				will_it_snow: 0,
-				chance_of_snow: 0,
-				vis_km: 10,
-				vis_miles: 6,
-				gust_mph: 15,
-				gust_kph: 24,
-				uv: 5,
+				time_epoch: currentEpochTime,
+				location: { tz_id: 'Pacific/Auckland' },
 			},
 		]
 
-		// using spread operator in actual code so do same here for test
-		const expectedOutput: HourForecast[] = [
-			{
-				...mockData[0],
-				time_epoch: '1:20 PM', // expected AM/PM format
-			},
-		]
+		// run convertHours function then store result
+		const result = convertHours(mockData, 'Pacific/Auckland')[0].time_epoch
 
-		// assertions
-		expect(convertHours(mockData)).toEqual(expectedOutput)
+		// result should now match AM/PM format (e.g. "hh:mm am/pm")
+		expect(result).toMatch(/^\d{2}:\d{2} (am|pm)$/)
 	})
 })
